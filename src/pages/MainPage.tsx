1dbec1601fc52d990f3e82fdc06547fd80e6a173
Duanyuqi397 <641045913@qq.com>
2022-07-24 22:38:14 +0800

    fix:操作出现了一丝瑕疵 重新提交

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,49 @@
-import { Button } from "antd";
+import { Row, Col } from "antd";
+import "./MainPage.css";
+import importComponents from "../scripts/importComponents";
+
+//引入components下的组件
+const components:Record<string,any> = importComponents(
+  require.context("../components/", false, /[^.]+\.tsx/)
+);
 
 export const MainPage = () => {
-    return <Button type='primary'>beautiful-resume</Button>
+  return (
+    <>
+      <div>Header</div>
+      <div>
+        <Row>
+          <Col span={4}>
+            <div className="component-lib block">
+              <div className="component-lib-title">组件库</div>
+              <ul>
+                {Object.keys(components).map((item, index) => {
+                  return <li className="component-item" draggable={true} onDrag={onDrag}  key={index}>{item}</li>;
+                })}
+              </ul>
+            </div>
+          </Col>
+          <Col span={16}>
+            <div className="view-area block">
+              <div className="view-area-title">预览区域</div>
+              {Object.values(components)?.map((Component:any,index:any) => (
+                <li key={index}>
+                    <Component />
+                </li>
+              ))}
+            </div>
+          </Col>
+          <Col span={4}>
+            <div className="config-panel block">
+              <div className="config-panel-title">属性配置</div>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+};
+
+const onDrag = () => {
+
 }
