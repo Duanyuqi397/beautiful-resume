694e155c0448854ff70f6c6366fee3ad0f1469ab
Duanyuqi397 <641045913@qq.com>
2022-07-27 23:33:18 +0800

    chore:修改了渲染引擎,有两个ts类型错误待解决

--- src/fragments/renderEngine.ts
+++ src/fragments/renderEngine.ts
@@ -1,5 +1,21 @@
-const renderEngine = () => {
+import React from "react";
+import { Component } from "../types/types";
 
+const renderEngine = (components: Component[]) => {
+    const componentsMap = new Map();
+    components.map(component => componentsMap.set(component.id, component));
+    return renderComponent(components[0], componentsMap);
+}
+
+const renderComponent = (component: Component, componentsMap: Map<string, Component>) => {
+    if (component.type === "text") {
+        return component.props?.valueOf;
+    }
+    const children: any = component.children.length
+        && component.children
+            .map(childId => componentsMap.get(childId))
+            .map(child => renderComponent(child, componentsMap));
+    return React.createElement(component.type, component.props, children);
 }
 
 export default renderEngine;
