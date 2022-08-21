bb80336feb6a15584fb4510e134af941d2563086
Duanyuqi397 <641045913@qq.com>
2022-08-21 14:55:03 +0800

    add: style of component-lib

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,9 @@
 import { Row, Col } from "antd";
 import "./MainPage.css";
 import importComponents from "../scripts/importComponents";
-import { Component } from "../types/types";
+import { Component, ConfigProps } from "../types/types";
 import renderEngine, { RenderEngine } from "../fragments/renderEngine";
+import ConfigPanel from "../fragments/ConfigPanel";
 import { useComponents, useMap } from "../fragments/dataHook";
 import {
   useState,
@@ -13,31 +14,33 @@ import {
   RefObject,
 } from "react";
 import Draggable, { Position } from "../fragments/Draggable";
+import useEvent from "../fragments/eventHook";
+import { useConfig } from "../fragments/configEngine";
+import importConfigs from "../scripts/importConfigs";
+import importIcons from "../scripts/importIcons";
 
 //引入components下的组件
 const components: Record<string, any> = importComponents(
   require.context("../components/", false, /[^.]+\.tsx/)
 );
 
+//引入配置文件
+const configs: Record<string, any> = importConfigs(
+  require.context("../components/", false, /[^.]+\.ts$/)
+);
+
+//引入icon
+const icons: Record<string, any> = importIcons(
+  require.context("../assets/", false, /[^.]+\.svg$/)
+);
+
 const render = new RenderEngine(components)
-
-function useEvent(callback: (id: string, event: SyntheticEvent) => void) {
-  const callbackRef = useRef(callback);
-
-  useLayoutEffect(() => {
-    callbackRef.current = callback;
-  });
-
-  return (id: string, event: SyntheticEvent) => {
-    callbackRef.current(id, event);
-  };
-}
 
 const defaultRoot: Omit<Component, "id" | "children"> = {
   type: "div",
   props: {
     id: "root-container",
-    style: { backgroundColor: "pink", position: "relative", height: "842px",width: "595px",margin: 'auto' },
+    style: { backgroundColor: "white", position: "relative", height: "842px",width: "595px",margin: 'auto' },
   },
 };
 
@@ -61,37 +64,47 @@ function offsetSet(container: HTMLElement, element: HTMLElement) {
   ];
 }
 
-export const MainPage = () => {
+export const MainPage = () => { 
+  console.log(icons);
+  console.log(components);
+  const [element,operation] = useConfig(configs);
   const position = useMap<number, [number, number]>(new Map());
   const containerRef = useRef<HTMLElement | null>(null);
   const nameRef = useRef<Map<number, HTMLElement | null>>(new Map());
   const [elements, op] = useComponents(defaultRoot, {
     common: {
-      onMouseDown: useEvent((id, e) => {}),
+        onMouseDown: useEvent((id, e) => {}),
 
-      onMouseUp: useEvent((id, e) => {}),
+        onMouseUp: useEvent((id, e) => {}),
 
-      onDrag: useEvent((id, position) => {
-        op.mergePropsTo("drag", id, { position: position as any });
-      }),
-    },
-  });
+        onPositionChange: useEvent((id, position) => {
+            op.mergePropsTo('drag', id, {position: position as any})
+        }),
+
+        onSizeChange: useEvent((id, size) => {
+            op.mergePropsTo('drag', id, {size: size as any})
+        })
+      }
+    }
+  )
 
   useEffect(() => {
-    const container = document.getElementById("root-container");
-    containerRef.current = container;
-  }, []);
+    const container = document.getElementById("root-container")
+    containerRef.current = container
+  }, [])
 
-  function addComponent(type: string, left: number, top: number):Component {
+  function addComponent(type: string, left: number, top: number) {
     return op.add(type, {
       style: { left, top, position: "absolute" },
       drag: {
-        bound: "parent",
+        bound: 'parent',
+        canResize: true,
         canDrag: true,
         position: [0, 0],
-        disableArea: 10,
-      },
-    });
+        size: { width: 0, height: 0 },
+        disableArea: 10
+      }
+    })
   }
 
   function onDragEnd(index: number, type: string) {
@@ -102,28 +115,46 @@ export const MainPage = () => {
     const containByContainer = contains(containerRef.current, element);
     if (containByContainer) {
       const [left, top] = offsetSet(containerRef.current, element);
-      const config = addComponent(type, left, top);
-      debugger
+      const newComponent = addComponent(type, left, top);
+      const currentConfig = operation.addConfig(newComponent);
     }
     position.set(index, [0, 0]);
   }
 
   return (
-    <>
-      <div>Header</div>
+    <div>
+      <div className="header">编辑自定义组件</div>
       <div>
         <Row>
           <Col span={4}>
             <div className="component-lib block">
-              <Draggable>
-                <div className="component-lib-title">组件库</div>
-              </Draggable>
-              <ul>
-                {Object.keys(components).map((item, index) => {
+                <div className="component-lib-title">基础组件</div>
+              <div className="component-list">
+                {Object.keys(icons).map((item,index) => {
+                    return (
+                      <Draggable
+                        position={position.get(index) || [0, 0]}
+                        onPositionChange={([x, y]) => position.set(index, [x, y])}
+                        onDragEnd={() => onDragEnd(index, item)}
+                        key={index}
+                        disableArea={10}
+                      >
+                        <div
+                          ref={(r) => nameRef.current.set(index, r)}
+                          className="component-item"
+                          key={index}
+                        >
+                          <img src={icons[item]}/>
+                          {item}
+                        </div>
+                      </Draggable>
+                    );
+                  })}
+                {/* {Object.keys(components).map((item, index) => {
                   return (
                     <Draggable
                       position={position.get(index) || [0, 0]}
-                      onDrag={([x, y]) => position.set(index, [x, y])}
+                      onPositionChange={([x, y]) => position.set(index, [x, y])}
                       onDragEnd={() => onDragEnd(index, item)}
                       key={index}
                       disableArea={10}
@@ -135,41 +166,32 @@ export const MainPage = () => {
                         // onDrag={onDrag}
                         // onDragEnd={onDropOver}
                         key={index}
-                        style={{cursor: 'pointer'}}
                         //onClick = {() => addComponent(item)}
                       >
                         {item}
                       </li>
                     </Draggable>
                   );
-                })}
-              </ul>
+                })} */}
+              </div>
             </div>
           </Col>
           <Col span={16}>
             <div className="view-area block">
-              <div className="view-area-title">预览区域</div>
-              {/* {Object.values(components)?.map((Component:any,index:any) => (
-                <li key={index}>
-                    <Component />
-                </li>
-              ))} */}
+              {/* <div className="view-area-title">预览区域</div> */}
               {render.render(op.getMap())}
             </div>
           </Col>
           <Col span={4}>
             <div className="config-panel block">
               <div className="config-panel-title">属性配置</div>
+              <div className="config">
+                <ConfigPanel {...element} />
+              </div>
             </div>
           </Col>
         </Row>
       </div>
-    </>
+    </div>
   );
-};
-
-const onDrag = () => {};
-
-const onDropOver = () => {
-  console.log(11111);
 };
